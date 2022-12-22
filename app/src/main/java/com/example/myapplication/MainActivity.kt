5950f6923ac0c1e6682ae368108141ee635f8bb6
Nitesh Kumar <niteshman30@gmail.com>
2022-12-22 18:39:47 +0530

    some changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,57 +8,75 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    private lateinit var newRecyclerView: RecyclerView
-    private  lateinit var newArrayList: ArrayList<News>
-    lateinit var imageId:Array<Int>
-    lateinit var heading:Array<String>
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var list:ArrayList<News>
+
+     lateinit var adapter: MyAdapter
+//    private lateinit var newRecyclerView: RecyclerView
+//    private  lateinit var newArrayList: ArrayList<News>
+//    lateinit var imageId:Array<Int>
+//    lateinit var heading:Array<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
         with(binding){
-           imageId= arrayOf(
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img,
-               R.drawable.img
-           )
-            heading= arrayOf(
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india",
-                "A Day will come when i will be prime minister of india"
+          list= ArrayList()
+            for (i in 0..10)(
+                    list.add(News(R.drawable.img,"A Day will come when i will be prime minister of india"))
             )
 
-            newRecyclerView.layoutManager=LinearLayoutManager(this@MainActivity)
+        adapter= MyAdapter(list)
+            recyclerview.adapter=adapter
+            recyclerview.layoutManager=LinearLayoutManager(this@MainActivity)
 
-            newArrayList= arrayListOf<News>()
-            getUserdata()
+
 
         }
+
+//        with(binding){
+//           imageId= arrayOf(
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img,
+//               R.drawable.img
+//           )
+//            heading= arrayOf(
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india",
+//                "A Day will come when i will be prime minister of india"
+//            )
+//
+//            newRecyclerView.layoutManager=LinearLayoutManager(this@MainActivity)
+//
+//            newArrayList= arrayListOf<News>()
+//            getUserdata()
+//
+//        }
     }
 
-    private fun getUserdata() {
-        for(i in imageId.indices){
-            val news=News(imageId[i],heading[i])
-            newArrayList.add(news)
-        }
-        newRecyclerView.adapter=MyAdapter(newArrayList)
-    }
+//    private fun getUserdata() {
+//        for(i in imageId.indices){
+//            val news=News(imageId[i],heading[i])
+//            newArrayList.add(news)
+//        }
+//        newRecyclerView.adapter=MyAdapter(newArrayList)
+//    }
 }
